9cedb515e485469dda5dd5ff097aaf8000d72a2e
Yash Kumar Jha <yashkumarjha20@gmail.com>
2025-08-16 13:02:56 +0530

    re-push

--- src/components/local/articleDetails/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header';
-export { default as DiscussionSection } from './DiscussionSection';
-export { default as ArticleContentSection } from './ArticleContentSection';
-export { default as ExploreMoreSection } from './ExploreMoreSection';
-export { default as ArticleScrollProgress } from './ArticleScrollProgress';
