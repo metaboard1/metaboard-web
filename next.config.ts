5c7589a0d2b644454a1831a95549647e03f85a43
Yash Kumar Jha <100863383+Yash-dott@users.noreply.github.com>
2025-08-28 18:10:17 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ["api.metaboard.in", "picsum.photos","metaboard-assets.s3.ap-south-1.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.metaboard.in",
+      },
+      {
+        protocol: "https",
+        hostname: "picsum.photos",
+      },
+      {
+        protocol: "https",
+        hostname: "metaboard-assets.s3.ap-south-1.amazonaws.com",
+      },
+    ],
   },
-
 };
 
 export default nextConfig;
